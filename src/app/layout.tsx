3d1a0bfc9896f5184b2dd42eb68bd688cbddae1a
Hamza <hamzaoubrahim51@gmail.com>
2023-12-08 18:09:40 +0100

    fix that sussy bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
             <div className="w-full flex flex-row justify-between items-center pb-4">
               <div className="flex flex-row gap-6">
                 <Link href="/" className={`link-primary`}>about</Link>
-                <Link href="/blogs" className={`link-primary`} prefetch>blogs</Link>
+                <Link href="/blogs" className={`link-primary`} >blogs</Link>
               </div>
 
               <ModeToggle />
